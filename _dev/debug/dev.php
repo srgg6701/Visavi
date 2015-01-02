70dca2ea93eef027168283f9bffda290fa96a23c
srgg6701 <srgg67@gmail.com>
2015-01-02 19:40:38 +0300

    Подключение скриптов отладки.

---
+++ _dev/debug/dev.php
@@ -0,0 +1,203 @@
+<!--
+1. Добавить директиву подключения этого файла на тестируемую стр. перед тестируемым блоком (обычно - 1-й уровень внутри BODY)
+include_once '[path]/dev.php'; -->
+<!--
+2. Подключить jQuery, jQueryUI
+-->
+<?php /*
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+<?php
+*/
+// директория поделючения тестового скрипта
+$path_to_debug = false;
+/**
+ * Настройки подложки: */
+// Идентификатор главного тестируемого блока:
+$main_block = false;//"'#page'";
+// Изображения для страниц:
+$substrates = array(    // класс => имя файла изображения
+    'default'=>'substrate.png'
+);?>
+<style>
+    #controls {
+        background-color: white;
+        -webkit-box-shadow: <?php echo $box_shadow;?>;
+        -moz-box-shadow: <?php echo $box_shadow;?>;
+        box-shadow: <?php echo $box_shadow;?>;
+        padding: 2px 4px;
+        position: fixed;
+        right: 50%;
+        padding: 10px;
+        padding-top: 0;
+        -webkit-transform: translateX(50%);
+        -moz-transform: translateX(50%);
+        -ms-transform: translateX(50%);
+        -o-transform: translateX(50%);
+        opacity: 0;
+        transform: translateX(50%);
+        white-space: nowrap;
+        z-index: 2;
+    }
+    #controls:hover {
+        opacity: 1;
+    }
+    #opacity-range {
+        margin-left: 20px;
+        width: 80px;
+    }
+    .sbstr {
+        background-repeat: no-repeat;
+        height: 568px;
+        margin: -36px;
+        opacity: 0.5;
+        position: absolute;
+        top: 31px;
+        width: 100%;
+        z-index: -1
+    }
+    #substrate-wrapper {
+        bottom: 0;
+        margin: auto;
+        max-width: 1100px;
+        position: absolute;
+        top: 0;
+        z-index: -1;
+    }
+    <?php   // установить ширину блока с подложкой
+            foreach($substrates as $class=>$substrate):
+                $sPath = $substrate_path . $substrate;
+                if(!file_exists($sPath)) $wrongPaths[]=$sPath;
+                ?>
+    #substrate.<?php echo $class;?> {
+        background: url(<?php echo $sPath;?>) no-repeat;
+        background-position-x: -242px;
+    }
+    <?php   endforeach;?>
+    #substrate {
+        height: 2000px;
+        margin: 0 auto;
+        max-width: 1366px;
+    }
+    .error_warning{
+        color:red;
+    }
+</style>
+<?php
+if(isset($wrongPaths)):
+    foreach ($wrongPaths as $wPath) {?>
+    <div class="error_warning">
+        Не найден файл подложки <?php echo $wPath;?>
+    </div>
+<?php
+    }
+
+endif;
+//
+$section = $_GET['section'];
+if(!$section) $section='default';
+
+if(!$path_to_debug){?>
+    <div class="error_warning"><b>Ошибка!</b>
+        <p>Не указана директория подключения тестового скрипта ($path_to_debug)</p>
+    </div>
+<?
+}
+if(!$main_block){?>
+    <div class="error_warning"><b>Ошибка!</b>
+        <p>Не указан идентификатор контейнера для тестирования ($main_block)</p>
+    </div>
+<?
+}
+
+// Путь извлечения изображений:
+$substrate_path = $path_to_debug.'/pixel-perfect/';
+// Тени
+$box_shadow = '0 4px 8px rgba(0, 0, 0, 0.5), 0 -14px 20px 2px rgba(0, 0, 0, 0.1) inset';
+?>
+<div id="controls">
+<?php   $show_substrate=true; // не показывать подложку
+
+        if($_GET['sbstr']=='false')
+            $show_substrate=false;
+
+        elseif(!$show_substrate&&isset($_GET['sbstr']))
+            $show_substrate=true;
+
+        if($show_substrate):?>
+    <label>
+        <input type="checkbox" id="sbstr"<?php
+        $opacity=(isset($_GET['opa']))? $_GET['opa']:0;
+        if($opacity>0):?> checked="checked" <?php endif;?> />Подложка
+    </label>
+    <label title="Прозрачность подложки">
+        <input type="range" id="opacity-range" min="0"  max="100" value="<?php echo $opacity*100;?>" />
+    </label>
+    &nbsp;
+    <label title="Прозрачность контента">
+        <input type="range" id="opacity-range-content" min="0"  max="100" value="100" />
+    </label>
+    &nbsp;
+<?php   endif;?>
+    <a href="?section=auto-profile">Профайл авто</a>
+    &nbsp;
+    <a href="?section=search-result">Результат поиска</a>
+</div>
+<?php if($show_substrate):?>
+<div id="substrate-wrapper">
+    <div style="opacity: <?php echo $opacity;?>" id="substrate" class="<?php echo $section;?>"></div>
+</div>
+<?php endif;?>
+<script>
+    jQuery(function(){
+        var $=jQuery,
+            page = $(<?php echo $main_block;?>),
+            checkbox =$('#sbstr'),
+            substrate = $('#substrate'),
+            range = $('#opacity-range'),
+            tested_content = $('#opacity-range-content');
+        // вычислить отступ слева для подложки
+        var w = window.document.getWidth(),
+            sbOffset=((w-$(page).width())/ 2)/w * 100 + '%',
+            setRange = function(){
+                if($(substrate).is(':visible'))
+                    $(range).val(parseInt($(substrate).css('opacity'))*100);
+                else
+                    $(range).val('0');
+            },
+            changeOpacity = function(input){
+                return parseInt(input.value)/100
+            };
+        console.log('sbOffset: '+sbOffset);
+        $('#substrate-wrapper').css({
+            left:sbOffset,
+            right:sbOffset
+        });
+
+        $(checkbox).on('click', function(){
+            // если подложка скрыта
+            if($(substrate).css('opacity')==0){
+                $(substrate).css('opacity',1);
+                $(tested_content).val('50').trigger('input');
+                setRange();
+            }else{
+                $(substrate).fadeToggle(200,setRange);
+            }
+        });
+        $(range).on('input', function(){
+            var cbox=$(checkbox)[0], opa = changeOpacity(this);
+            if(opa>0) {
+                if(!cbox.checked) cbox.checked=true;
+            }else
+                cbox.checked=false;
+            $(substrate).css({
+                display:'block',
+                opacity:opa
+            });
+        });
+        $(tested_content).on('input', function(){
+            $(page).css('opacity', changeOpacity(this));
+        });
+        //$('#controls').draggable();
+    });
+</script>
---
+++ _dev/debug/rulers.php
@@ -0,0 +1,88 @@
+<?php   if(JRequest::getVar('rulers')):?>
+<?php   $rh = 'rulers-horizontal'; $rv = 'rulers-vertical';
+?>
+<style>
+    body{
+        position: relative;
+    }
+    #<?=$rh?>, #<?=$rv?>{
+        background-color: red;
+        cursor: move;
+        opacity: 0.5;
+        position: absolute;
+        z-index: 10;
+    }
+    #<?=$rh?>{
+        height: 35px;
+        top:35px;
+        width: 100%;
+    }
+    #<?=$rv?>{
+        bottom:0;
+        left: 35px;
+        top:0;
+        width: 35px;
+    }
+</style>
+<!--<script src="http://code.jquery.com/jquery.min.js"></script>-->
+<script>
+    jQuery(function(){
+        var rh = '<?=$rh?>',
+            rv = '<?=$rv?>',
+            diff= 0,
+            attrDr='data-dragged',
+            horizontal=$('#'+rh),
+            vertical=$('#'+rv);
+        // вычислить разницу между позициями линейки и мыши
+        var moveRulers = function (event){
+            var diff, obj = event.currentTarget;
+            //console.log(event.currentTarget);
+            $(obj).attr(attrDr,1);
+            if(obj.id==rh){
+                diff=event.clientY-parseInt($(obj).css('top'));
+                //console.log('Down, diff H:'+diff);
+            }
+            if(obj.id==rv){
+                diff=event.clientX-parseInt($(obj).css('left'));
+                //console.log('Down, diff: V'+diff);
+            }
+        };
+        // блокировать выделение элементов страницы
+        $('body *').on('selectstart', function(){
+            //console.log(event.currentTarget);
+            if($(horizontal).attr(attrDr)||$(vertical).attr(attrDr))
+                return false;
+        });
+        // переместить/деактивировать линейки
+        $('body').on('mousemove mouseup', function(event){
+            //console.log('event.type = ' + event.type);
+            switch (event.type){
+                case 'mousemove':
+                    if($(horizontal).attr(attrDr)){
+                        $(horizontal).css({
+                            top:(event.clientY-diff)+window.scrollY+'px'
+                        }); //console.log('clientY:'+event.clientY+'\n');
+                    }
+                    if($(vertical).attr(attrDr)){
+                        $(vertical).css({
+                            left:(event.clientX-diff)+'px'
+                        }); //console.log('clientX:'+event.clientX);
+                    }
+                    break;
+                case 'mouseup':
+                    $(horizontal).removeAttr(attrDr);
+                    $(vertical).removeAttr(attrDr);
+                    break;
+            }
+        });
+        $(horizontal).on('mousedown',function(event){
+            moveRulers(event);
+        });
+        $(vertical).on('mousedown',function(event){
+            moveRulers(event);
+        });
+    })(jQuery);
+</script>
+<div id="<?=$rh?>"></div>
+<div id="<?=$rv?>"></div>
+<?php   endif;?>
