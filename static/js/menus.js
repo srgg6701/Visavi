75b925d0c2245dd474fa6ccd70eae6fde9e2d808
srgg6701 <srgg67@gmail.com>
2015-01-08 20:24:29 +0300

    Полностью модифицирован и протестирован обработчик боковых меню.

--- static/js/menus.js
+++ static/js/menus.js
@@ -1,23 +1,74 @@
 $(function(){
+    // получить ширину меню и body
+    // вычислить ширину #page
+    var body=$('body'),
+        container=$('#page'),
+        leftMenu=$('#left-side'),
+        rightMenu=$('#right-side'),
+        leftWidth=$(leftMenu).outerWidth(),
+        rightWidth=$(rightMenu).outerWidth(),
+        commonHeight=$(container).height()+'px',
+        duration=200,
+        getBodyMarginLeft=function(right){
+            var offset=leftWidth;
+            if(right) offset*=2;
+            //console.log('%cleftWidth = '+offset,'color: violet');
+            return   '-'+offset+'px';
+        };
+    // Явно задать ширину контейнера с контентом
+    $(container).css('width',$(body).outerWidth());
+    // Задать высоту боковых меню
+    $(leftMenu).css('height',commonHeight);
+    $(rightMenu).css('height',commonHeight);
+    // Задать ширину body
+    $(body).css({
+        marginLeft:getBodyMarginLeft(),
+        width:$(container).width()+leftWidth+rightWidth+'px'
+    });
+    // Отобразить меню
+    $(leftMenu).show();
+    $(rightMenu).show();
+    //console.log('leftWidth: '+leftWidth+'\nrightWidth: '+rightWidth+'\npageWidth: '+$(container).css('width')+'\nbody.width: '+$(body).css('width'));
     // показать/спрятать мобильное меню
     $('#menu-left, #menu-right').on('click', function(){
-        //console.log(this);
-        var body=$('body'),
-            show_left_menu='show-left-menu',
-            show_right_menu='show-right-menu',
-            first_class=show_left_menu,
-            second_class=show_right_menu;
         // если клацали по кнопке управления меню слева
-        if(this.id=='menu-left'){
-            first_class=show_right_menu;
-            second_class=show_left_menu;
+        var shadow=$('<div/>').css({
+            background:   'black',
+            bottom:             '0',
+            opacity:            '0.35',
+            position:           'fixed',
+            top:                '0',
+            zIndex:             '1'
+            //left, right:      ?
+        });
+        $(container).prepend(shadow);
+        // Если состояние меню - по умолчанию
+        if($(body).css('margin-left')==getBodyMarginLeft()){
+            var mrgLeft,Left,Right;
+            if(this.id=='menu-left'){
+                mrgLeft=0;
+                Left=leftWidth+'px';
+                Right=0;
+            }else{
+                mrgLeft=getBodyMarginLeft(true);
+                Left=0;
+                Right=rightWidth+'px';
+            }
+            // выдвинуть левое/правое меню
+            $(body).animate({ marginLeft: mrgLeft},duration, function(){
+                // навести тень
+                $(shadow).css({
+                    left:Left,
+                    right:Right
+                })
+            });
         }
-        if($(body).hasClass(first_class)){
-            // показать меню слева
-            $(body).removeClass(first_class)
-                .addClass(second_class);
-        }else{ // переключить показ меню слева
-            $(body).toggleClass(second_class);
-        }
+        // Убрать меню и тени
+        var dropMenus=function(){
+            $(shadow).remove();
+            $(body).animate({ marginLeft: getBodyMarginLeft()},duration);
+        };
+        $(shadow).on('click', dropMenus);
+        //$([leftMenu.get(0),rightMenu.get(0)]).on('mouseleave', dropMenus);
     });
 });
