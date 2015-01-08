05400d42624def5915a467bc1795a929b2041876
srgg6701 <srgg67@gmail.com>
2015-01-08 14:19:28 +0300

    Изменён подход к управлению мобильными меню.

--- static/js/menus.js
+++ static/js/menus.js
@@ -2,7 +2,7 @@ $(function(){
     // показать/спрятать мобильное меню
     $('#menu-left, #menu-right').on('click', function(){
         //console.log(this);
-        var main=$('main'),
+        var body=$('body'),
             show_left_menu='show-left-menu',
             show_right_menu='show-right-menu',
             first_class=show_left_menu,
@@ -12,12 +12,12 @@ $(function(){
             first_class=show_right_menu;
             second_class=show_left_menu;
         }
-        if($(main).hasClass(first_class)){
+        if($(body).hasClass(first_class)){
             // показать меню слева
-            $(main).removeClass(first_class)
+            $(body).removeClass(first_class)
                 .addClass(second_class);
         }else{ // переключить показ меню слева
-            $(main).toggleClass(second_class);
+            $(body).toggleClass(second_class);
         }
     });
 });
