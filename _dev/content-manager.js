183034e31dbe52b39a7551a583f87d108100acef
srgg6701 <srgg67@gmail.com>
2015-01-19 09:36:22 +0300

    Оптимизация загрузчика контента.

--- _dev/content-manager.js
+++ _dev/content-manager.js
@@ -1,8 +1,8 @@
 $(function(){
-    //
-    var tail      = location.href.lastIndexOf('/')+1,
-        urlBase   = location.href.substring(0,tail),
-        params    = location.href.substr(tail);
+    // распарсить URL и получить сегменты
+    var baseParams = getBaseUrl(),
+        urlBase   = baseParams.urlBase,
+        params    = baseParams.params;
     //console.log('location.href = '+location.href);
     // todo: remove on production
     // ----------------------------------------------------
@@ -22,19 +22,19 @@ $(function(){
                 // загрузить тестовый копонент
                 $('body').prepend(data);
                 // загрузить контент
-                loadTemplate(urlBase);
+                loadTemplate();
             });
     }else{ // загрузить контент
         console.log('No url params...');
-        loadTemplate(urlBase);
+        loadTemplate();
     }
 });
 // info: удалить (если используются серверные сценарии) или модифицировать (если используется JS-framework).
 // Разобраться с загружаемым шаблоном
-function loadTemplate(urlBase){
+function loadTemplate(){
     var segment_pos,
-        section='1280_default',
-        tmpl = 'templates/'; // шаблон, загружаемый по умолчанию
+        section='1280_default';/*,
+        tmpl = 'templates/'; // шаблон, загружаемый по умолчанию*/
     if((segment_pos = location.href.indexOf('='))!=-1){ // если найдено, возвращает позицию, с которой будем начинать извлекать section (после сдвига на 1)
         segment_pos+=1; // скорректировать позицию
         section=(location.href.indexOf('&')!=-1)?
@@ -48,37 +48,36 @@ function loadTemplate(urlBase){
     }
     //console.log('url: templates/'+section+'.html');
     // Загрузить шаблон в #content
-    $('#content').load(tmpl+section+'.html', function(){
-        console.log('section='+section);
-        // добавить левую панель
+    getComponentArea().load(getPath(true)+section+'.html', function(){
+        //console.log('section='+section);
+        // добавить левую панель в main
         if (section.indexOf('1280_')!=-1)
-            loadLeftPanel(urlBase,section);
+            loadLeftPanel(section);
     });
 }
 // подгружить контент в левую панель
-function loadLeftPanel(urlBase,section){
+function loadLeftPanel(section){
 
     console.log('section = ' + section);
 
-    var main = $('main'),
-        leftPanel=$('<div/>', { class: 'left-panel' }),
-        path = 'templates/subtemplates/';
+    var urlBase = getBaseUrl().urlBase,
+        main = $('main'),
+        leftPanel=$('<div/>', { class: 'left-panel' });
 
     // добавить к main левую панель
     $(main).prepend(leftPanel);
 
-    // если страница по умолчанию, загрузить модуль "Популярные услуги"
-    if(section=='1280_default'){
-        // загрузить модули в левое меню
-        $.get(urlBase + path+'popular.html', function (data) {
-            $(leftPanel).append(data);
-            $.get(urlBase + path+'actions.html', function (data){
+    // загрузить модули в левое меню
+    switch(section){
+        case '1280_default': // если страница по умолчанию, загрузить модуль "Популярные услуги"
+            $.get(urlBase + getPath()+'popular.html', function (data) {
                 $(leftPanel).append(data);
-                $.get(urlBase + path+'achievement.html', function (data){
-                    $(leftPanel).append(data);
-                });
+                loadModule(leftPanel,'actions','achievement');
             });
-        });
+            break;
+        case '1280_actions': // акции и скидки
+            loadModule(leftPanel,'discounts','achievement');
+            break;
     }
     // если любая страница кроме загружаемой по умолчанию, добавить breadcrumbs
     if(section!='1280_default'){
@@ -86,7 +85,45 @@ function loadLeftPanel(urlBase,section){
             breadcrumbs=$('<div/>',{
                 class:bread_class
             });
-        $(main).prepend(breadcrumbs);
-        $('.'+bread_class).load( tmpl + 'subtemplates/'+bread_class+'.html');
+        getComponentArea().prepend(breadcrumbs);
+        $('.'+bread_class).load( getPath()+bread_class+'.html');
     }
+}
+/**
+ * Загрузить модули
+ */
+function loadModule(leftPanel,module,module_next){
+    $.get(getBaseUrl().urlBase + getPath() +module+'.html', function (data){
+        $(leftPanel).append(data);
+        if(module_next)
+            loadModule(leftPanel,module_next);
+    });
+}
+/**
+ * Получить и распарсить параметры Url
+ * @returns {{tail: number, urlBase: string, params: string}}
+ */
+function getBaseUrl(){
+    var tail      = location.href.lastIndexOf('/')+1,
+        urlBase   = location.href.substring(0,tail),
+        params    = location.href.substr(tail);
+    return{
+        tail:tail,
+        urlBase:urlBase,
+        params:params
+    }
+}
+/**
+ * Получить путь извлечения шаблонов
+ */
+function getPath(short){
+    var tmpl = 'templates/';
+    if(!short) tmpl+='subtemplates/';
+    return tmpl;
+}
+/**
+ * Получить область компонента
+ */
+function getComponentArea(){
+    return $('#content');
 }
