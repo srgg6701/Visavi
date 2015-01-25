6eb685f211a7ac89b788be24aa2cd03a22cb8fc5
srgg6701 <srgg67@gmail.com>
2015-01-25 21:34:13 +0300

    Создан обратный вызов функции.

--- templates/default/js/common.js
+++ templates/default/js/common.js
@@ -3,13 +3,47 @@ jQuery(function($){
         var diigo;
         if(diigo=$('[id^="diigo"]')){
             $(diigo).remove();
-            console.log('Set null? ─ ' + diigo);
+            //console.log('Set null? ─ ' + diigo);
             clearInterval(intId);
         }
     },250);
     //----------------------------------
-    $('.finder-search').on('click', function(){
+    $('label.finder-search').on('click', function(){
         console.log('submitting...');
         $(this).parents('form').eq(0).submit();
     });
+    function setInt(callback){
+        var search_cell,
+            cell_id = 'mod-finder-searchword',
+            doInput=setInterval(function() {
+            if (search_cell = document.getElementById(cell_id)) {
+                callback(search_cell);
+                clearInterval(doInput);
+            }
+        },100);
+    }
+    function setSearchType(search_cell){
+        search_cell.type='search';
+    }
+    // получить и сохранить ячейку поиска, назначить цвет текста для неё
+    var setSearchColor = function(){
+            setInt(setSearchType);
+            // переопределить функцию
+            setSearchColor =function(color){
+                (color)? console.log('color: '+color) : console.log('no color...');
+                console.log('color: '+color);
+                search_cell.style.color='#'+color;
+                console.dir(search_cell);
+            };
+            return cell_id;
+        },
+        // вернуть id объекта, назначить тип search (асинхронно) и переопределить функцию
+        cell_id=setSearchColor();
+    console.log('cell_id: '+cell_id);
+
+        $('#'+cell_id).on('blur',function(event){
+            var color=(event.type=='blur')? '999':'333';
+            console.log('event: '+event.type);
+            setSearchColor(color);
+        });
 }(jQuery));
