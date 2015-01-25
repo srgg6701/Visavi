cd3b8be5ac6bc241d942e191a881734aa262058a
srgg6701 <srgg67@gmail.com>
2015-01-25 21:53:25 +0300

    Улучшен обработчик ячейки.

--- templates/default/js/common.js
+++ templates/default/js/common.js
@@ -12,38 +12,38 @@ jQuery(function($){
         console.log('submitting...');
         $(this).parents('form').eq(0).submit();
     });
-    function setInt(callback){
-        var search_cell,
-            cell_id = 'mod-finder-searchword',
-            doInput=setInterval(function() {
+    // установить тип ячейки как search
+    setInt(setSearchType);
+    // обработать цвет текста
+    setInt(handleSearchCell);
+}(jQuery));
+
+// Вызывать callback-функцию
+function setInt(callback){
+    var search_cell,
+        cell_id = 'mod-finder-searchword',
+        doInput=setInterval(function() {
             if (search_cell = document.getElementById(cell_id)) {
                 callback(search_cell);
                 clearInterval(doInput);
             }
         },100);
-    }
-    function setSearchType(search_cell){
-        search_cell.type='search';
-    }
-    // получить и сохранить ячейку поиска, назначить цвет текста для неё
-    var setSearchColor = function(){
-            setInt(setSearchType);
-            // переопределить функцию
-            setSearchColor =function(color){
-                (color)? console.log('color: '+color) : console.log('no color...');
-                console.log('color: '+color);
-                search_cell.style.color='#'+color;
-                console.dir(search_cell);
-            };
-            return cell_id;
-        },
-        // вернуть id объекта, назначить тип search (асинхронно) и переопределить функцию
-        cell_id=setSearchColor();
-    console.log('cell_id: '+cell_id);
-
-        $('#'+cell_id).on('blur',function(event){
-            var color=(event.type=='blur')? '999':'333';
-            console.log('event: '+event.type);
-            setSearchColor(color);
-        });
-}(jQuery));
+}
+// установить тип ячейки как search и цвет текста по умолчанию
+function setSearchType(search_cell){
+    search_cell.type='search';
+    setSearchColor(search_cell);
+}
+//установить цвет текста ячейки поиска
+function setSearchColor(search_cell,color){
+    if(!color) color='999';
+    search_cell.style.color='#'+color;
+}
+// обработать цвет текста в зависимости от события ячейки
+function handleSearchCell(search_cell){
+    $(search_cell).on('blur click keypress',function(event){
+        var color=(event.type=='blur')? false:'333';
+        console.log('event: '+event.type);
+        setSearchColor(search_cell,color);
+    });
+}
