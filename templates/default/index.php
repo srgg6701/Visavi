d470cd0447894e01c37c866350f13f0e51ac0728
srgg6701 <srgg67@gmail.com>
2015-01-25 16:07:17 +0300

    Добавление вывода метатегов.

--- templates/default/index.php
+++ templates/default/index.php
@@ -7,11 +7,23 @@ $app = JFactory::getApplication();
 $leftbar = 1;
 $rightbar = 1;
 $tmpl_path = $this->baseurl . "/templates/" . $this->template . "/";
+if($show=JFactory::getApplication()->input->get('reveal')):
+    $reveal=$show=='all' ? $this : $this->$show;
+    echo "<div style='background-color: whitesmoke; padding:20px; border: solid 3px #ccc;'><pre>";
+    var_dump($reveal);
+    echo '</pre><hr>file: ' . __FILE__ . '<br>line: '.__LINE__.'<hr></div>';
+endif;
+$document=JFactory::getDocument();
 ?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>">
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <jdoc:include type="head" />
+    <?php /*<jdoc:include type="head" />*/ ?>
+    <title><?php echo $document->title;?></title>
+    <meta name="description" content="<?php
+        echo str_replace('"',"'",$document->getMetadata('description'));?>">
+    <meta name="keywords" content="<?php
+        echo str_replace('"',"'",$document->getMetadata('keywords')); ?>">
     <link rel="stylesheet" href="<?=$tmpl_path?>css/default.css" type="text/css" />
     <script src="<?=$tmpl_path?>js/min/jquery.min.js"></script>
     <script src="<?=$tmpl_path?>js/min/menus.min.js"></script>
@@ -21,52 +33,53 @@ $tmpl_path = $this->baseurl . "/templates/" . $this->template . "/";
 	<div id="message">
 	  <jdoc:include type="modules" name="message" style="xhtml" />
 	</div>
-	<header>
-        <section id="roof">
-        <?php   //
-                if ($this->countModules('top-menu')):?>
-            <jdoc:include type="modules" name="top-menu" style="none" />
-        <?php   endif;
-                //
-                if ($this->countModules('search')) : ?>
-            <div>
-            <jdoc:include type="modules" name="search" style="none" />
-            </div>
-        <?php   endif;
-                //
-                if ($this->countModules('cards')) : ?>
-            <jdoc:include type="modules" name="cards" style="none" />
-        <?php   endif;?>
-        </section>
-        <section id="visavi">
-            <div id="discount" class="color-pink">
-        <?php   //
-                if ($this->countModules('logotype')) : ?>
-            <jdoc:include type="modules" name="logotype" style="none" />
-        <?php   endif;
-                //
-                if ($this->countModules('discounts-on-banner')) : ?>
-            <jdoc:include type="modules" name="discounts-on-banner" style="none" />
-        <?php   endif;?>
-            </div>
-        <?php   //
-                if ($this->countModules('contacts')) : ?>
-            <jdoc:include type="modules" name="contacts" style="none" />
-        <?php   endif;?>
-        </section>
-        <section id="banner">
-            <div id="slider">
-                <div id="slider-controls">
-                    <div><span>◄</span></div>
-                    <div><span>■</span></div>
-                    <div><span>►</span></div>
+    <div id="page">
+	    <header>
+            <section id="roof">
+            <?php   //
+                    if ($this->countModules('top-menu')):?>
+                <jdoc:include type="modules" name="top-menu" style="none" />
+            <?php   endif;
+                    //
+                    if ($this->countModules('search')) : ?>
+                <div>
+                <jdoc:include type="modules" name="search" style="none" />
                 </div>
-            </div>
-        <?php   //
-                if ($this->countModules('trinity-buttons')) : ?>
-        <jdoc:include type="modules" name="trinity-buttons" style="none" />
-        <?php   endif;?>
-        </section>
+            <?php   endif;
+                    //
+                    if ($this->countModules('cards')) : ?>
+                <jdoc:include type="modules" name="cards" style="none" />
+            <?php   endif;?>
+            </section>
+            <section id="visavi">
+                <div id="discount" class="color-pink">
+            <?php   //
+                    if ($this->countModules('logotype')) : ?>
+                <jdoc:include type="modules" name="logotype" style="none" />
+            <?php   endif;
+                    //
+                    if ($this->countModules('discounts-on-banner')) : ?>
+                <jdoc:include type="modules" name="discounts-on-banner" style="none" />
+            <?php   endif;?>
+                </div>
+            <?php   //
+                    if ($this->countModules('contacts')) : ?>
+                <jdoc:include type="modules" name="contacts" style="none" />
+            <?php   endif;?>
+            </section>
+            <section id="banner">
+                <div id="slider">
+                    <div id="slider-controls">
+                        <div><span>◄</span></div>
+                        <div><span>■</span></div>
+                        <div><span>►</span></div>
+                    </div>
+                </div>
+            <?php   //
+                    if ($this->countModules('trinity-buttons')) : ?>
+            <jdoc:include type="modules" name="trinity-buttons" style="none" />
+            <?php   endif;?>
+            </section>
         <?php   //
                 if ($this->countModules('roof-mobile')) : ?>
         <jdoc:include type="modules" name="roof-mobile" style="none" />
@@ -83,32 +96,33 @@ $tmpl_path = $this->baseurl . "/templates/" . $this->template . "/";
                 if ($this->countModules('main-menu')) : ?>
         <jdoc:include type="modules" name="main-menu" style="none" />
         <?php   endif;?>
-	</header>
-	<main id="content" role="main">
-        <?php   //
-                if ($this->countModules('breadcrumbs')) : ?>
-        <jdoc:include type="modules" name="breadcrumbs" style="none" />
-        <?php   endif;?>
-        <div class="left-panel">
-        <?php   // популярные услуги
-                if ($this->countModules('popular-services')) : ?>
-        <jdoc:include type="modules" name="popular-services" style="none" />
-        <?php   endif;
-                // лазерная эпиляция
-                if ($this->countModules('laser-epilation')) : ?>
-        <jdoc:include type="modules" name="laser-epilation" style="none" />
-        <?php   endif;
-                // акции и скидки
-                if ($this->countModules('actions-and-discounts')) : ?>
-        <jdoc:include type="modules" name="actions-and-discounts" style="none" />
-        <?php   endif;
-                // наши заслуги
-                if ($this->countModules('our-achievements')) : ?>
-        <jdoc:include type="modules" name="our-achievements" style="none" />
-        <?php   endif;?>
-        </div>
-		<jdoc:include type="component" />
-	</main>
+	    </header>
+        <main id="content" role="main">
+            <?php   //
+                    if ($this->countModules('breadcrumbs')) : ?>
+            <jdoc:include type="modules" name="breadcrumbs" style="none" />
+            <?php   endif;?>
+            <div class="left-panel">
+            <?php   // популярные услуги
+                    if ($this->countModules('popular-services')) : ?>
+            <jdoc:include type="modules" name="popular-services" style="none" />
+            <?php   endif;
+                    // лазерная эпиляция
+                    if ($this->countModules('laser-epilation')) : ?>
+            <jdoc:include type="modules" name="laser-epilation" style="none" />
+            <?php   endif;
+                    // акции и скидки
+                    if ($this->countModules('actions-and-discounts')) : ?>
+            <jdoc:include type="modules" name="actions-and-discounts" style="none" />
+            <?php   endif;
+                    // наши заслуги
+                    if ($this->countModules('our-achievements')) : ?>
+            <jdoc:include type="modules" name="our-achievements" style="none" />
+            <?php   endif;?>
+            </div>
+            <jdoc:include type="component" />
+        </main>
+    </div>
 	<footer>
 		<?php   //
                 if ($this->countModules('footer-notice')) : ?>
@@ -127,5 +141,50 @@ $tmpl_path = $this->baseurl . "/templates/" . $this->template . "/";
         <jdoc:include type="modules" name="social-buttons" style="none" />
         <?php   endif;?>
 	</footer>
+    <?php
+$test_stylesheets='test_css';
+$test_js='test_js';
+/*
+Joomla:
+$css_test=JFactory::getSession()->get($test_stylesheets);
+*/
+$inc_tests=false;
+$css_test=isset($_SESSION[$test_stylesheets])||isset($_GET[$test_stylesheets]);
+$js_test= isset($_SESSION[$test_js])||isset($_GET[$test_js]);
+    if($inc_tests||$css_test||$js_test):?>
+<script>
+    var sheet,
+        sheets = document.querySelectorAll('link[href][rel="stylesheet"]'),
+        script,
+        scripts = document.querySelectorAll('script[src]');
+    <?php
+    if($css_test):?>
+    for (var i in sheets) {
+        sheet = sheets[i];
+        if (sheet.href)
+            console.log(sheet.href);
+    }
+    console.log('%cTo turn these messages off, add the "<?=$test_stylesheets?>=-1" param to the URL.', 'color: blue;');
+    <?php
+    else:?>
+    console.log('%cAdd the "<?=$test_js?>" param to the URL to see attached stylesheets.', 'color: violet;');
+    <?php
+    endif;
+    if($js_test):?>
+    for (var i in scripts) {
+        script = scripts[i];
+        if (script.src)
+            console.log(script.src);
+    }
+    console.log('%cTo turn these messages off, add the "<?=$test_js?>=-1" param to the URL.', 'color: green;');
+    <?php
+    else:?>
+    console.log('%cAdd the "<?=$test_js?>" param to the URL to see attached js.', 'color: brown;');
+    <?php
+    endif;?>
+</script>
+    <?php
+    endif;
+    ?>
   </body>
 </html>
--- templates/default/js/common.js
+++ templates/default/js/common.js
@@ -1,1 +1,10 @@
-console.log('JS is included!');
+jQuery(function($){
+    var intId=setInterval(function(){
+        var diigo;
+        if(diigo=$('[id^="diigo"]')){
+            $(diigo).remove();
+            console.log('Set null? ─ ' + diigo);
+            clearInterval(intId);
+        }
+    },250);
+}(jQuery));
